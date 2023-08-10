5223f6c4a2998c513923827d08bd2b6b4997e259
SweetB0nes <kontorskihroma@gmail.com>
2023-08-10 03:15:25 +0300

    create bot

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN='6439709813:AAF7WyN8Qhqu_Nbok6ZLfO6aklz9GDD0YMo'
+TOKEN='Xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 model_path = './model'
 model_name = 'cointegrated/rut5-small-chitchat'
